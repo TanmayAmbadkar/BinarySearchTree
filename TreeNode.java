7972e9a1f6939634ee39c082e350384ae6f86adb
201851026-iiitv <201851026@iiitvadodara.ac.in>
2019-04-23 01:28:59 +0530

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,18 +1,17 @@
-public class TreeNode<Type>
-{
-    TreeNode left;
-    TreeNode right;
-    Type val;
-    TreeNode()
-    {
-        left=null;
-        right=null;
-        val=null;
+public class Tree {
+    Integer val;
+    Tree lc;
+    Tree rc;
+
+    Tree() {
+        val = null;
+        lc = null;
+        rc = null;
     }
-    TreeNode(Type val)
-    {
-        left=null;
-        right=null;
-        this.val=val;
+
+    Tree(Integer a) {
+        val = a;
+        lc = null;
+        rc = null;
     }
 }
