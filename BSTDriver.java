1301eb5a09cac4cced1f1ecb92f2bfe4b41b9b89
201851026-iiitv <201851026@iiitvadodara.ac.in>
2019-04-23 01:29:50 +0530

    Update BSTDriver.java

--- BSTDriver.java
+++ BSTDriver.java
@@ -1,31 +1,12 @@
-public class BSTDriver
-{
-    public static void main(String args[])
-    {
-        BinarySearchTree bst=new BinarySearchTree(75);
-        bst.insert(100);
-        bst.insert(45);
-        bst.insert(120);
-        bst.insert(99);
-        bst.insert(32);
-        bst.insert(35);
-        bst.insert(48);
-        bst.insert(98);
-        bst.insert(-100);
-        bst.insert(76);
-        bst.insert(74);
-        bst.insert(57);
-        bst.insert(73);
-        bst.inOrderTraversal();
+public class DriverBST {
+    public static void main(String args[]) {
+        BST bst = new BST(100);
+        bst.InsertElement(45);
+        bst.InsertElement(30);
+        bst.InOrderTraverse(bst.root);
         System.out.println("\b");
-        bst.delete(57);
-        bst.inOrderTraversal();
-        System.out.println("\b");
-        bst.delete(75);
-        bst.inOrderTraversal();
-        System.out.println("\b");
-        bst.delete(73);
-        bst.inOrderTraversal();
+        bst.DeleteElement(100);
+        bst.InOrderTraverse(bst.root);
         System.out.println("\b");
     }
-}       
+}
